4b0173272e719cc6655b87eb262721458ec8a5a9
WoernerBro <seanwoerner.ru89@gmail.com>
2017-09-16 14:57:39 -0500

    Final Version: -Reordered functions. -Renamed 1 or 2 variables. -Further shortened functions by adding additional functions.

--- src/misc/RecordProcessor.java
+++ src/misc/RecordProcessor.java
@@ -16,50 +16,45 @@ public class RecordProcessor {
 
 	public static String processFile(String fileName) {
 		try {
-			inputFile = openFile(fileName);			
+			readFromFile(fileName);
 		} catch (FileNotFoundException fileNotFound) {
 			throw new RuntimeException("File Not Found:" + fileName);
 		}
 		
-		int numberOfRecords = getNumberOfRecords();
-		initializeRecords(numberOfRecords);
+		int numberOfRecords = validateRecords();
+		
+		if(numberOfRecords <= 0) {
+			System.err.println("No records found in data file");
+			throw (new RuntimeException("No records found in data file"));
+		}
+		
+		inputFile.close();
+		return createReport().toString();
+	}
+	
+	private static void readFromFile(String fileName) throws FileNotFoundException {
+		int numberOfRecords = 0;
 		try {
 			inputFile = openFile(fileName);			
 		} catch (FileNotFoundException fileNotFound) {
 			throw new RuntimeException("File Not Found:" + fileName);
 		}
 		
-		numberOfRecords = validateRecords();
-		if(numberOfRecords <= 0) {
-			System.err.println("No records found in data file");
-			throw (new RuntimeException("No records found in data file"));
-		}
-		inputFile.close();
-		return createReport().toString();
-	}
-	
-	public static int getNumberOfRecords() {
-		int numberOfRecords = 0;
 		while(inputFile.hasNextLine()) {
 			String record = inputFile.nextLine();
 			if(record.length() > 0)
 				numberOfRecords++;
 		}
-		return numberOfRecords;
-	}
-	
-	public static StringBuffer createReport() {
-		StringBuffer stringBuffer = new StringBuffer();
-
-		stringBuffer.append(printData());
-		stringBuffer.append(printAverages());
-		stringBuffer.append(findUniqueNames(firstName, "First"));
-		stringBuffer.append(findUniqueNames(lastName, "Last"));
+		initializeRecords(numberOfRecords);
 		
-		return stringBuffer;
+		try {
+			inputFile = openFile(fileName);			
+		} catch (FileNotFoundException fileNotFound) {
+			throw new RuntimeException("File Not Found:" + fileName);
+		}
 	}
 	
-	public static Scanner openFile(String fileName) throws FileNotFoundException {
+	private static Scanner openFile(String fileName) throws FileNotFoundException {
 		try {
 			return new Scanner(new File(fileName));
 		} catch (RuntimeException fileNotFound) {
@@ -68,7 +63,7 @@ public class RecordProcessor {
 		}
 	}
 
-	public static void initializeRecords(int numberOfRecords) {
+	private static void initializeRecords(int numberOfRecords) {
 		firstName = new String[numberOfRecords];
 		lastName = new String[numberOfRecords];
 		age = new int[numberOfRecords];
@@ -77,36 +72,33 @@ public class RecordProcessor {
 		inputFile.close();
 	}
 
-	public static void processRecord(String record, int numberOfRecords) {
-		String [] recordItems = record.split(",");
-	
-		int insertionIndex = insertRecord(recordItems, numberOfRecords);
+	private static int validateRecords() {
+		int numberOfRecords = 0;
+		String record;
 		
-		firstName[insertionIndex] = recordItems[0];
-		lastName[insertionIndex] = recordItems[1];
-		employeeType[insertionIndex] = recordItems[3];
-	
-		try {
-			age[insertionIndex] = Integer.parseInt(recordItems[2]);
-			pay[insertionIndex] = Double.parseDouble(recordItems[4]);
-		} catch(NumberFormatException failureToParseData) {
-			System.err.println("Incorrect age or payment format for entry:\n" + firstName[insertionIndex] + " " + lastName[insertionIndex]);
-			System.err.println(failureToParseData.getMessage());
-			throw(failureToParseData);
+		while(inputFile.hasNextLine()) {
+			record = inputFile.nextLine();
+			processRecord(record, numberOfRecords);
+			numberOfRecords++;
+		}
+		
+		return numberOfRecords;
+	}
+
+	private static void processRecord(String record, int numberOfRecords) {
+		if(record.length() > 0) {
+			String [] recordItems = record.split(",");
+		
+			int insertionIndex = insertRecord(recordItems, numberOfRecords);
+			
+			firstName[insertionIndex] = recordItems[0];
+			lastName[insertionIndex] = recordItems[1];
+			employeeType[insertionIndex] = recordItems[3];
+			validateNumberItems(insertionIndex, recordItems);
 		}
 	}
 
-	public static void moveRecords(int j, int numberOfRecords) {
-		for(int i = numberOfRecords; i > j; i--) {
-			firstName[i] = firstName[i - 1];
-			lastName[i] = lastName[i - 1];
-			age[i] = age[i - 1];
-			employeeType[i] = employeeType[i - 1];
-			pay[i] = pay[i - 1];
-		}
-	}
-
-	public static int insertRecord(String [] recordItems, int numberOfRecords) {
+	private static int insertRecord(String [] recordItems, int numberOfRecords) {
 		int insertionIndex; 
 		for(insertionIndex = 0; insertionIndex < lastName.length; insertionIndex++) {
 			if(lastName[insertionIndex] == null)
@@ -120,8 +112,64 @@ public class RecordProcessor {
 		
 		return insertionIndex;
 	}
+	
+	private static void validateNumberItems(int insertionIndex, String [] recordItems) {
+		try {
+			age[insertionIndex] = Integer.parseInt(recordItems[2]);
+			pay[insertionIndex] = Double.parseDouble(recordItems[4]);
+		} catch(NumberFormatException failureToParseData) {
+			System.err.println("Incorrect age or payment format for entry:\n" 
+					+ firstName[insertionIndex] + " " + lastName[insertionIndex]);
+			System.err.println(failureToParseData.getMessage());
+			inputFile.close();
+			throw(failureToParseData);
+		}
+	}
 
-	public static String printAverages() {
+	private static void moveRecords(int insertionIndex, int numberOfRecords) {
+		for(int i = numberOfRecords; i > insertionIndex; i--) {
+			firstName[i] = firstName[i - 1];
+			lastName[i] = lastName[i - 1];
+			age[i] = age[i - 1];
+			employeeType[i] = employeeType[i - 1];
+			pay[i] = pay[i - 1];
+		}
+	}
+
+	private static StringBuffer createReport() {
+		StringBuffer stringBuffer = new StringBuffer();
+	
+		stringBuffer.append(printData());
+		stringBuffer.append(printAverages());
+		stringBuffer.append(findUniqueNames(firstName, "First"));
+		stringBuffer.append(findUniqueNames(lastName, "Last"));
+		
+		return stringBuffer;
+	}
+
+	private static String printData() {
+		String output = "";
+		output += String.format("# of people imported: %d\n", firstName.length);
+		output += String.format("\n%-30s %s  %-12s %12s\n", "Person Name", "Age", "Emp. Type", "Pay");
+		
+		for(int i = 0; i < 30; i++)
+			output += String.format("-");
+		output += String.format(" ---  ");
+		for(int i = 0; i < 12; i++)
+			output += String.format("-");
+		output += String.format(" ");
+		for(int i = 0; i < 12; i++)
+			output += String.format("-");
+		output += String.format("\n");
+		
+		for(int i = 0; i < firstName.length; i++)
+			output += String.format("%-30s %-3d  %-12s $%12.2f\n", 
+					firstName[i] + " " + lastName[i], age[i], employeeType[i], pay[i]);
+		
+		return output;
+	}
+
+	private static String printAverages() {
 		String output = "";
 		
 		float sumOfAges = 0;
@@ -137,7 +185,7 @@ public class RecordProcessor {
 		return output;
 	}
 
-	public static double findAverageByEmployeeType(String payType) {
+	private static double findAverageByEmployeeType(String payType) {
 		double averagePay = 0;
 		int numEmployeeType = 0;
 		for(int i = 0; i < lastName.length; i++)
@@ -149,21 +197,34 @@ public class RecordProcessor {
 		return averagePay/(double)numEmployeeType;
 	}
 
-	public static String findUniqueNames(String [] nameArray, String nameType) {
+	private static String findUniqueNames(String [] nameArray, String nameType) {
 		String output = "";
 		HashMap<String, Integer> uniqueNames = new HashMap<String, Integer>();
+		int numberOfUniqueNames = findNumberOfNames(uniqueNames, nameArray);
 		
-		int numUniqueNames = findNumberOfNames(uniqueNames, nameArray);
 		output += String.format("\n" + nameType + " names with more than one person sharing it:\n");
-		if(numUniqueNames > 0) {
-			output += displayUniqueNames(uniqueNames, numUniqueNames);
+		if(numberOfUniqueNames > 0) {
+			output += displayUniqueNames(uniqueNames, numberOfUniqueNames);
 		} else
 			output += String.format("All " + nameType + " names are unique\n");
 		
 		return output;
 	}
 
-	public static String displayUniqueNames(HashMap<String, Integer> uniqueNames, int numUniqueNames) {
+	private static int findNumberOfNames(HashMap<String, Integer> uniqueNames, String [] nameArray) {
+		int numberOfUniqueNames = 0;
+		for(int i = 0; i < nameArray.length; i++) {
+			if(uniqueNames.containsKey(nameArray[i])) {
+				uniqueNames.put(nameArray[i], uniqueNames.get(nameArray[i]) + 1);
+				numberOfUniqueNames++;
+			} else
+				uniqueNames.put(nameArray[i], 1);
+		}
+		
+		return numberOfUniqueNames;
+	}
+
+	private static String displayUniqueNames(HashMap<String, Integer> uniqueNames, int numUniqueNames) {
 		String output = "";
 		Set<String> allUniqueNames = uniqueNames.keySet();
 		for(String name : allUniqueNames)
@@ -171,61 +232,5 @@ public class RecordProcessor {
 				output += String.format("%s, # people with this name: %d\n", name, uniqueNames.get(name));
 		
 		return output;
-	}
-	
-	public static int findNumberOfNames(HashMap<String, Integer> uniqueNames, String [] nameArray) {
-		int numUniqueNames = 0;
-		for(int i = 0; i < nameArray.length; i++) {
-			if(uniqueNames.containsKey(nameArray[i])) {
-				uniqueNames.put(nameArray[i], uniqueNames.get(nameArray[i]) + 1);
-				numUniqueNames++;
-			} else
-				uniqueNames.put(nameArray[i], 1);
-		}
-		
-		return numUniqueNames;
-	}
-	
-	public static String printData() {
-		String output = "";
-		output += String.format("# of people imported: %d\n", firstName.length);
-		
-		output += String.format("\n%-30s %s  %-12s %12s\n", "Person Name", "Age", "Emp. Type", "Pay");
-		for(int i = 0; i < 30; i++)
-			output += String.format("-");
-			output += String.format(" ---  ");
-		for(int i = 0; i < 12; i++)
-			output += String.format("-");
-		output += String.format(" ");
-		for(int i = 0; i < 12; i++)
-			output += String.format("-");
-		output += String.format("\n");
-		
-		for(int i = 0; i < firstName.length; i++)
-			output += String.format("%-30s %-3d  %-12s $%12.2f\n", firstName[i] + " " + lastName[i], age[i]
-				, employeeType[i], pay[i]);
-		
-		return output;
-	}
-	
-	public static int validateRecords() {
-		int numberOfRecords = 0;
-		
-		while(inputFile.hasNextLine()) {
-			String record = inputFile.nextLine();
-			if(record.length() > 0) {
-				try {
-					processRecord(record, numberOfRecords);
-				}
-				catch (NumberFormatException failureToParseData){
-					inputFile.close();
-					throw(failureToParseData);
-				}
-				
-				numberOfRecords++;
-			}
-		}
-		
-		return numberOfRecords;
 	}
 }
